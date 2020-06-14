472040db38734ecb957986c78bca93e2ba742480
3x1t1um <54857145+3x1t1um@users.noreply.github.com>
2020-06-14 15:36:37 +0200

    Create informations.py

---
+++ src/informations.py
@@ -0,0 +1,9 @@
+mercury = '{"name": "Mercury","mass": 3.3011e23,"radius": 2439.7,"distance": 57910000,"speed": 87.969,"texture": "img/mercury.jpg"}'
+venus = '{"name": "Venus","mass": 4.8675e24,"radius": 6051.8,"distance": 108200000,"speed": 225,"texture": "img/venus.jpg"}'
+earth = '{"name": "Earth","mass": 5.9724e24,"radius": 6371,"distance": 149600000,"speed": 365.6,"texture": "img/earth.jpg"}'
+mars = '{"name": "Mars","mass": 6.4171e23,"radius": 3389.5,"distance": 227900000,"speed": 669.58,"texture": "img/mars.jpg"}'
+
+jupiter = '{"name": "Jupiter","mass": 1.89819e27,"radius": 69911,"distance": 778500000,"speed": 4335.3545,"texture": "img/jupiter.jpg"}'
+saturn = '{"name": "Saturn","mass": 5.6834e26,"radius": 58232,"distance": 1434000000,"speed": 10757.7365,"texture": "img/saturn.jpg"}'
+uranus = '{"name": "Uranus","mass": 8.6813e25,"radius": 25362,"distance": 2871000000,"speed": 30687.15,"texture": "img/uranus.jpg"}'
+neptune = '{"name": "Neptune","mass": 1.02413e26,"radius": 24622,"distance": 4495000000,"speed": 60224.9036,"texture": "img/neptune.jpg"}'
